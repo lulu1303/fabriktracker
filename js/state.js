6481106ab2fa4925b7395f75a5302bbe6271ddc8
lulu1303 <lukas@veitl.de>
2026-08-19 16:54:21 +0200

    Update state.js

--- js/state.js
+++ js/state.js
@@ -5,26 +5,3 @@ let selectedPart = null;
 let searchTimer = null;
 
 
-/*
- * NEU:
- * Hier speichern wir die aktuellen
- * Suchergebnisse separat.
- *
- * Dadurch müssen wir keine kompletten
- * JavaScript-Objekte mehr direkt in
- * onclick schreiben.
- */
-
-let legoSearchResults = [];
-
-
-/* =========================================================
-   PREIS
-========================================================= */
-
-const PRICE_PER_100G = 11;
-
-const PRICE_PER_GRAM =
-  PRICE_PER_100G / 100;
-
-const DISCOUNT = 0.20;
