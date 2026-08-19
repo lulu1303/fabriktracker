054a20a38c1f85cdc6dd3a6467f27c07528eebb4
lulu1303 <lukas@veitl.de>
2026-08-19 16:36:27 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,21 @@
+/* =========================================================
+   SUPABASE
+========================================================= */
+
+const SUPABASE_URL =
+  "https://nmsoppdibanqfaeqtube.supabase.co";
+
+const SUPABASE_KEY =
+  "sb_publishable_rcf_aY1AfeyRmumUDiNhBw_7LUWdVBD";
+
+const PARTS_URL =
+  SUPABASE_URL +
+  "/rest/v1/parts";
+
+const LEGO_PARTS_URL =
+  SUPABASE_URL +
+  "/rest/v1/lego_parts";
+
+const WEIGHTS_URL =
+  SUPABASE_URL +
+  "/rest/v1/lego_part_weights";
