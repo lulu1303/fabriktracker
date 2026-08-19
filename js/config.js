f0448b363d806822084ed69656d123fbdd38c403
lulu1303 <lukas@veitl.de>
2026-08-19 17:02:59 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,25 +8,20 @@ const SUPABASE_URL =
 const SUPABASE_KEY =
   "sb_publishable_rcf_aY1AfeyRmumUDiNhBw_7LUWdVBD";
 
-
 const PARTS_URL =
   SUPABASE_URL +
   "/rest/v1/parts";
-
 
 const LEGO_PARTS_URL =
   SUPABASE_URL +
   "/rest/v1/lego_parts";
 
-
 const WEIGHTS_URL =
   SUPABASE_URL +
   "/rest/v1/lego_part_weights";
 
-
 const PRICE_PER_GRAM =
   0.11;
-
 
 const DISCOUNT =
   0.2;
